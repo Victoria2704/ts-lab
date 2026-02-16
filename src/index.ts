4b0aaa958ca37257004fe7d84265a8be587041a1
Замашанская Виктория <cuff84cuff@gmail.com>
2026-02-16 23:02:50 +0700

    Task 1: User interface + createUser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("TS works!");
+interface User {
+    id: number;
+    name: string;
+    email: string | undefined;
+    isActive: boolean;
+}
+
+function createUser (
+    id: number,
+    name: string,
+    email: string | undefined,
+    isActive: boolean = true
+): User {
+    return { id, name, email, isActive};
+}
+
+const user1 = createUser(1, "Victoria", undefined);
+const user2 = createUser(2, "Vika", "vika@mail.com", false);
+console.log("Task1:", user1, user2);

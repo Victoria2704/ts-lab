52c1c4f41146143df8a1a53077f2450194761581
Замашанская Виктория <cuff84cuff@gmail.com>
2026-03-17 01:52:00 +0700

    add groupBy helper

--- src/lab4.ts
+++ src/lab4.ts
@@ -6,6 +6,7 @@ export type { GroupBy } from "./lab4/groupBy";
 export type { GroupTransform } from "./lab4/having";
 export type { Having } from "./lab4/having";
 
+export { createGroupBy } from "./lab4/groupBy";
 export { createSort } from "./lab4/sort";
 export { createWhere } from "./lab4/where";
 export { query } from "./lab4/query";
--- src/lab4/groupBy.ts
+++ src/lab4/groupBy.ts
@@ -4,3 +4,25 @@ import type { Transform } from "./transform";
 export type GroupBy<T extends object> = <K extends keyof T>(
   key: K
 ) => Transform<T, Group<T, K>>;
+
+export function createGroupBy<T extends object>(): GroupBy<T> {
+  return (key) => (items) => {
+    const groups = new Map<T[typeof key], Group<T, typeof key>>();
+
+    for (const item of items) {
+      const groupKey = item[key];
+      const group = groups.get(groupKey);
+
+      if (group) {
+        group.items.push(item);
+      } else {
+        groups.set(groupKey, {
+          key: groupKey,
+          items: [item],
+        });
+      }
+    }
+
+    return Array.from(groups.values());
+  };
+}

81b3ee3b080f496ad9069d1eef98e1e9172375b6
Замашанская Виктория <cuff84cuff@gmail.com>
2026-03-23 21:43:27 +0700

    add PickedByType type

--- src/lab6.ts
+++ src/lab6.ts
@@ -1,1 +1,2 @@
 export type { DeepReadonly } from "./lab6/deepReadonly.ts";
+export type { PickedByType } from "./lab6/pickedByType.ts";
---
+++ src/lab6/pickedByType.ts
@@ -0,0 +1,3 @@
+export type PickedByType<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
